7eef017b6610cf6bfc1862f7b570f1db904bc169
田志鹏 <tianzhipeng@zhongchuangsanyou.com>
2021-03-05 10:47:27 +0800

    更新 数据专制方法

--- vue/index.js
+++ vue/index.js
@@ -1,7 +1,9 @@
 import {assert} from '../lib/assert';
+import {d2l} from '../lib/dataToList';
 
 export default {
   install(Vue, options) {
-    Vue.prototype.$assert = assert
+    Vue.prototype.$assert = assert;
+    Vue.prototype.$d2l = d2l
   }
 }
