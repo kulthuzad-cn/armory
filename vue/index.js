31bf8a2144a8cd0bce957e9ab85064035344d8e4
田志鹏 <tianzhipeng@zhongchuangsanyou.com>
2021-03-29 15:59:05 +0800

    添加 部分 数组方法

--- vue/index.js
+++ vue/index.js
@@ -1,9 +1,11 @@
 import {assert} from '../lib/assert';
 import {d2l} from '../lib/dataToList';
+import arrExtend from '../lib/arrayExtend';
 
 export default {
   install(Vue, options) {
     Vue.prototype.$assert = assert;
-    Vue.prototype.$d2l = d2l
+    Vue.prototype.$d2l = d2l;
+    Vue.prototype.$arr = arrExtend
   }
 }
