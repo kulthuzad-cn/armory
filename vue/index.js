5d55f5ac2ea783dc9b23b3a6d925288422a0d4d4
田志鹏 <tianzhipeng@zhongchuangsanyou.com>
2021-03-30 18:55:08 +0800

    添加 部分 公共方法及扩展方法

--- vue/index.js
+++ vue/index.js
@@ -1,11 +1,15 @@
 import {assert} from '../lib/assert';
 import {d2l} from '../lib/dataToList';
 import arrExtend from '../lib/arrayExtend';
+import stringExtend from '../lib/stringExtend';
+import dateExtend from '../lib/dateExtend';
 
 export default {
   install(Vue, options) {
     Vue.prototype.$assert = assert;
     Vue.prototype.$d2l = d2l;
     Vue.prototype.$arr = arrExtend
+    Vue.prototype.$str = stringExtend
+    Vue.prototype.$date = dateExtend
   }
 }
